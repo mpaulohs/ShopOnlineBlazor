c7af8ac6085071a2fd2c67cf9ed6e69ee52a4eb5
developmentservice <igor.melnik.it@gmail.com>
2021-11-11 21:05:40 +0200

    Created UserRepository

---
+++ API/ModelsRepository/IEntityRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.ModelsRepository
+{
+    public interface IEntityRepository<TEntity>
+    {
+        public Task<TEntity> Create(TEntity entity);
+
+        public Task<ICollection<TEntity>> Read();
+
+        public Task<TEntity> Read(Guid id);
+
+        public Task<ICollection<TEntity>> Read(IDictionary<string, object> parameters);
+
+        public Task<TEntity> Update(TEntity entity);
+
+        public Task<TEntity> Update(Guid quid, object parameters);
+
+        public Task Delete();
+
+        public Task Delete(TEntity entity);
+
+        public Task Delete(Guid id);
+    }
+}
+
+
---
+++ API/ModelsRepository/UserRepository.cs
@@ -0,0 +1,151 @@
+﻿using API.ModelsRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ShopOnlinePWA.API.Migrations.AppIdentityDb;
+using ShopOnlinePWA.Library.Identity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopOnlinePWA.API.ModelsRepository
+{
+    public class UserRepository : IEntityRepository<User>
+    {
+        private readonly AppIdentityDbContext appIdentityDbContext;
+        private readonly ILogger logger;
+
+        public UserRepository(AppIdentityDbContext appIdentityDbContext, ILogger logger)
+        {
+            this.appIdentityDbContext = appIdentityDbContext;
+            this.logger = logger;
+        }
+
+        public async Task<User> Create(User entity)
+        {
+            try
+            {
+                var result = await appIdentityDbContext.Users.AddAsync(entity);
+                await appIdentityDbContext.SaveChangesAsync();
+                return result.Entity;
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "An error occurred while create user entity in database.");
+                throw new Exception();
+            }
+        }
+
+        public async Task<ICollection<User>> Read()
+        {
+            try
+            {
+                return await appIdentityDbContext.Users.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "An error occurred while read all users entity from database.");
+                throw new Exception();
+            }
+        }
+
+        public async Task<User> Read(Guid id)
+        {
+            try
+            {
+                return await appIdentityDbContext.Users.SingleOrDefaultAsync(e => e.Id == id);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while read user entity by id  from database.");
+                throw new Exception();
+            }
+        }
+
+        public async Task<ICollection<User>> Read(IDictionary<string, object> parameters)
+        {
+            try
+            {
+                //ToDo check this behavior
+                var users = appIdentityDbContext.Users;
+                foreach ((string key, object value) in parameters)
+                {
+                    users.Where(e => e.GetType().GetProperty(key).GetValue(e) == value);
+            
+                }
+                return users.ToList();
+               
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public Task<User> Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Update(Guid quid, object parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Delete()
+        {
+            try
+            {
+                foreach (var entity in appIdentityDbContext.Users)
+                {
+                    appIdentityDbContext.Users.Remove(entity);
+                }
+                await appIdentityDbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "An error occurred while deleting all users in database.");
+                throw new Exception();
+            }
+        }
+
+        public async Task Delete(User entity)
+        {
+            try
+            {
+                appIdentityDbContext.Users.Remove(entity);
+                await appIdentityDbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "An error occurred while deleting user in database.");
+                throw new Exception();
+            }
+        }
+
+        public async Task Delete(Guid id)
+        {
+            try
+            {
+                var entity = await appIdentityDbContext.Users
+                    .FirstOrDefaultAsync(e => e.Id == id);
+
+                if (entity != null)
+                {
+                    appIdentityDbContext.Users.Remove(entity);
+                    await appIdentityDbContext.SaveChangesAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "An error occurred while deleting user by guid in database.");
+                throw new Exception();
+            }
+        }
+
+    }
+}
