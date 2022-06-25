7ffab7fbb2338c58c178f094fea3fe5c4079045c
Igor Melnyk <igor.melnyk.it@gmail.com>
2022-06-26 00:16:38 +0100

    update seed.cs

--- Api/Data/SeedDb.cs
+++ Api/Data/SeedDb.cs
@@ -12,34 +12,47 @@ namespace Api.Data
         {
             var faker = new Faker();
             //Catalogs
-            var products = new List<Product<TKey>>();
+
+            var productCharacteristics = new List<ProductCharacteristic<TKey>>();
             for (int i = 0; i < length; i++)
             {
-                var productCharacteristic = new ProductCharacteristic<TKey>();
-                productCharacteristic.Id = (TKey)Convert.ChangeType(i, typeof(TKey));
+                var entity = new ProductCharacteristic<TKey>();
 
+                entity.Id = (TKey)Convert.ChangeType(i, typeof(TKey));
+                entity.Name = entity.Name;
+                entity.CreatedAt = faker.Date.Past(5);
+                entity.CreatedAt = faker.Date.Between(entity.CreatedAt, DateTime.Now);
+                entity.Comment = faker.Lorem.Sentence();
+                entity.ConcurrencyStamp = Guid.NewGuid().ToString();
+
+                productCharacteristics.Add(entity);
             }
 
+            var products = new List<Product<TKey>>();
 
             for (int i = 1; i < length; i++)
             {
-                var product = new Product<TKey>();
-                product.Id = (TKey)Convert.ChangeType(i, typeof(TKey));
-                product.FullName = faker.Commerce.Product();
-                product.Name = product.FullName;
-                product.Comment = faker.Lorem.Sentence();
-                product.IsPublic = faker.Random.Bool();
-                product.Article = faker.Commerce.Ean13();
-                product.CreatedAt = faker.Date.Past(5);
-                product.Description = faker.Commerce.ProductDescription();
-                product.ConcurrencyStamp = Guid.NewGuid().ToString();
-                //product.ProductCharacteristic
+                var entity = new Product<TKey>();
 
-                products.Add(product);
+                entity.Id = (TKey)Convert.ChangeType(i, typeof(TKey));
+                entity.Name = faker.Commerce.Product();
+                entity.Name = entity.Name;
+                entity.FullName = faker.Commerce.Product();
+                entity.CreatedAt = faker.Date.Past(5);
+                entity.CreatedAt = faker.Date.Between(entity.CreatedAt, DateTime.Now);
+                entity.Comment = faker.Lorem.Sentence();
+                entity.ConcurrencyStamp = Guid.NewGuid().ToString();
 
-                
+                entity.IsPublic = faker.Random.Bool();
+                entity.Article = faker.Commerce.Ean13();
+                entity.Description = faker.Commerce.ProductDescription();
+
+                entity.ProductCharacteristic = faker.PickRandom<ProductCharacteristic<TKey>>(productCharacteristics);
+                entity.MainImageUrl = faker.Image.Random.ToString();
+                products.Add(entity);
             }
 
+            modelBuilder.Entity<ProductCharacteristic<TKey>>().HasData(productCharacteristics);
             modelBuilder.Entity<Product<TKey>>().HasData(products);
         }
     }
