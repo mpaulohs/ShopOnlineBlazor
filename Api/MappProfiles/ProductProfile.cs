0e71f15f1900dcf0c5828fbf033dd69041ad61ed
Igor Melnyk <igor.melnik.it@gmail.com>
2022-08-15 00:01:46 +0300

    api=>fix automapper di

--- Api/MappProfiles/ProductProfile.cs
+++ Api/MappProfiles/ProductProfile.cs
@@ -4,20 +4,12 @@ using Shared.Models.Catalogs;
 using Shared.Models.Dtos;
 namespace Api.MappProfiles
 {
-    class ProductProfile : Profile
+    public class ProductProfile : Profile
     {
         public ProductProfile()
         {
-            new MapperConfiguration(cfg => cfg.CreateProjection<Product<Guid>, ProductDTO<Guid>>());
-            // return await entities.ProjectTo<TOut>(configuration).ToListAsync<TOut>();
-            // new MapperConfiguration(conf =>
-            // {
-            //     conf.CreateMap<Product<Guid>, ProductDTO<Guid>>().ReverseMap();
-            //     conf.CreateProjection<Product<Guid>, ProductDTO<Guid>>()
-            //     .ForMember(dest => dest.ProductSerie, act => act
-            //     .MapFrom(source => source.ProductSerie.Name));
-            //     // .ForMember(destinationMember => destinationMember.ProductQuality, memberOptions => memberOptions.MapFrom(src => src.ProductQuality.Name));
-            // });
+            CreateMap<Product<Guid>, ProductDTO<Guid>>();
+            CreateProjection<Product<Guid>, ProductDTO<Guid>>();
         }
     }
 }
