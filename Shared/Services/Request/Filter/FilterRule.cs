839cfc5f30240e61ef0f591665cf27594f1c6fdf
Igor Melnyk <igor.melnik.it@gmail.com>
2022-08-17 13:29:09 +0300

    checked rest

--- Shared/Services/Request/Filter/FilterRule.cs
+++ Shared/Services/Request/Filter/FilterRule.cs
@@ -1,13 +1,15 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace Shared.Services.Request.Filter;
 public class FilterRule
 {
     public string PropertyName { get; set; }
     public FilterOperation Operation { get; set; }
     public object Value { get; set; }
-    public FilterRule(string propertyName, string symbol, object value)
+    public FilterRule(string propertyName, string shortName, object value)
     {
         PropertyName = propertyName;
-        Operation = FilterOperation.GetOperation(symbol: symbol);
+        Operation = FilterOperation.GetOperation(shortName: shortName);
         Value = value;
     }
 }
