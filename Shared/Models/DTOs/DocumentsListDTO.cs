01dfd6a886b92f5b2d3f3df79dde20b2c49cb075
Igor Melnyk <igor.melnik.it@gmail.com>
2022-08-29 01:42:53 +0300

    update doucements page autofill

--- Shared/Models/DTOs/DocumentsListDTO.cs
+++ Shared/Models/DTOs/DocumentsListDTO.cs
@@ -1,15 +1,29 @@
+using Bogus.DataSets;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace Shared.Models.Dtos;
 
 public class DocumentsListDTO<TKey>
     where TKey : IEquatable<TKey>
 {
+    [Display(Name = "Id", Order = 1)]
     public TKey Id { get; set; }
-    public string? Number { get; set; }
+    [Display(Name = "Date", AutoGenerateField = true, Order = 2)]
+    [DisplayFormat(DataFormatString = "{yyyy-MM-dd}")]
     public DateTime DateTime { get; set; }
+    [Display(Name = "Number", AutoGenerateField = true, Order = 3)]
+    public string? Number { get; set; }
+    [Display(Name = "Type", AutoGenerateField = true, Order = 4)]
     public string DocumentType { get; set; }
+    [Display(Name = "Status", AutoGenerateField = true, Order = 5)]
     public string DocumentStatus { get; set; }
+    [Display(Name = "Client", AutoGenerateField = true, Order = 6)]
     public string Client { get; set; }
+    [Display(Name = "Amount", AutoGenerateField = true, Order = 7)]
     public decimal DocumentAmount { get; set; }
+    [Display(Name = "Currency", AutoGenerateField = true, Order = 8)]
     public string DocumentCurrency { get; set; }
+    [Display(Name = "Comment", AutoGenerateField = true, Order = 9)]
     public string? Comment { get; set; }
 }
